26e7c11808a9c54c697f88e3b527d606ab45480c
sief aqel <sief.aqel@teklabz.com>
2025-10-15 09:02:29 +0300

    remove image kit

--- app/api/auth/imagekit/route.ts
+++ app/api/auth/imagekit/route.ts
@@ -1,13 +1,13 @@
-import config from "@/lib/config";
-import imageKit from "imagekit";
-import { NextResponse } from "next/server";
+// import config from "@/lib/config";
+// import imageKit from "imagekit";
+// import { NextResponse } from "next/server";
 
-const imagekit = new imageKit({
-  publicKey: config.env.imagekit.publicKey,
-  privateKey: config.env.imagekit.privateKey,
-  urlEndpoint: config.env.imagekit.urlEndpoint
-});
+// const imagekit = new imageKit({
+//   publicKey: config.env.imagekit.publicKey,
+//   privateKey: config.env.imagekit.privateKey,
+//   urlEndpoint: config.env.imagekit.urlEndpoint
+// });
 
-export async function GET() {
-  return NextResponse.json(imagekit.getAuthenticationParameters());
-}
+// export async function GET() {
+//   return NextResponse.json(imagekit.getAuthenticationParameters());
+// }
